8c3519b4d6166f7d311b7d3b8b4ce0bb40b08c23
Notebook <zekserj@ukr.net>
2024-01-16 16:41:03 +0200

    HW_4

---
+++ HW_4/hw_4_additionally.py
@@ -0,0 +1,38 @@
+data = [
+    [
+        {"id": 1110, "field": {}},
+        {"id": 1111, "field": {}},
+        {"id": 1112, "field": {}},
+        {"id": 1113, "field": {}},
+        {"id": 1114, "field": {}},
+        {"id": 1115, "field": {}},
+    ],
+    [
+        {"id": 1110, "field": {}},
+        {"id": 1120, "field": {}},
+        {"id": 1122, "field": {}},
+        {"id": 1123, "field": {}},
+        {"id": 1124, "field": {}},
+        {"id": 1125, "field": {}},
+    ],
+    [
+        {"id": 1130, "field": {}},
+        {"id": 1131, "field": {}},
+        {"id": 1122, "field": {}},
+        {"id": 1132, "field": {}},
+        {"id": 1133, "field": {}},
+    ]
+]
+
+res = []
+
+max_len = max(len(sublist) for sublist in data)
+
+for i in range(max_len):
+    for sublist in data:
+        if i < len(sublist):
+            current_id = sublist[i]["id"]
+            if current_id not in res:
+                res.append(current_id)
+
+print(res)
